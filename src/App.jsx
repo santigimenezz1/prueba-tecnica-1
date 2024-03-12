8fb82b87f613ec53bb095d850cc356ddf56a0c3b
santiago <santiago.manuel.gimenez97@gmail.com>
2024-03-12 11:57:15 +0100

    PRUEBA TECNICA 1 ORDENADA

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,42 @@ function App() {
   const [frase, setFrase] = useState("")
   const [url, setUrl] = useState("")
 
-   useEffect(()=>{  //aca hago la peticion, guardo la frase en un estado y obtengo la primera palabra 
+   
+  useEffect(()=>{  //aca hago la peticion, guardo la frase en un estado y obtengo la primera palabra 
     fetch("https://catfact.ninja/fact")
     .then((res)=>res.json())
     .then((res)=>{
       setFrase(res.fact)
-      console.log(res)
       const {fact} = res 
-      setFrase(fact)
-      const primeraPalabra = fact.split(" ")[0]
-      console.log(primeraPalabra)
-      //Aca tengo que hacer la peticion para buscar el objeto del gato y obtener el id para armar la url
-      fetch(`https://cataas.com/cat/says/${primeraPalabra}&json=true`)
-      .then((data)=>data.json())
-      .then((data)=>{  
-        console.log(data)     //una vez que contengo del objeto el id creo la url para guardarla en un estado y luego ponerla en el src de la imagen en el renderizado
-        const url = data._url
-        setUrl(`https://cataas.com/cat/says/${url}`)
-      })
-                                              
+      setFrase(fact)                                       
     })
    },[])
 
+       //Cuando cambia el estado frase se ejecuta este efecto
+
+   useEffect(()=>{//Aca tengo que hacer la peticion para buscar el objeto del gato y obtener el id para armar la url
+      if(!frase) return
+    const primeraPalabra = frase.split(" ")[0]
+      fetch(`https://cataas.com/cat/says/${primeraPalabra}&json=true`)
+      .then((data)=>data.json())
+      .then((data)=>{  
+        const url = data._url
+        setUrl(`https://cataas.com/cat/says/${url}`)
+      })
+   },[frase])
+
  
   return (
-    <>
-    <h1>{frase}</h1>
-    <img src={url}></img>
-    
+    <main className='main'>
+      <h1>App de gatos</h1>
+      <section className='section'>
+        <h1>{frase}</h1>
+        <img src={url} ></img>
+
+      </section>
+
+    </main>
   
-    </>
   )
 }
 
